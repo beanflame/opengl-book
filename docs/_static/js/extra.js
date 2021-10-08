c42028a03151ec0d52c8f7acf525877128a4badf
beanflame <68175497+beanflame@users.noreply.github.com>
2021-10-08 15:53:00 +0800

    Update extra.js

--- docs/_static/js/extra.js
+++ docs/_static/js/extra.js
@@ -7,23 +7,10 @@ window.addEventListener('load', function () {
   if (a) {
     document.body.setAttribute('data-md-color-accent', a);
   }
+  
+  var s = localStorage.getItem('data-md-color-scheme');
+  if (s) {
+    document.body.setAttribute('data-md-color-scheme', s);
+  }
+  
 }, false);
-
-/*
-var cur = document.getElementsByClassName('page-time')[0]
-if (cur) {
-  var comments = document.getElementById('gitment_container');
-  var pos = 0;
-  if (comments) {
-    pos -= comments.textContent.length;
-  }
-  comments = document.getElementById('__comments');
-  if (comments) {
-    pos -= comments.textContent.length;
-  }
-  var textLength = 0;
-  if (pos < 0) textLength = (document.getElementsByClassName('md-content__inner')[0].textContent.slice(0, pos).replace(/\s/g, '').length - document.getElementsByTagName('h1')[0].textContent.replace(/\s/g, '').length - document.getElementsByClassName('page-copyright')[0].textContent.replace(/\s/g, '').length - 4);
-  else textLength = (document.getElementsByClassName('md-content__inner')[0].textContent.replace(/\s/g, '').length - document.getElementsByTagName('h1')[0].textContent.replace(/\s/g, '').length - document.getElementsByClassName('page-copyright')[0].textContent.replace(/\s/g, '').length - 4);
-  if (textLength <= 0) cur.innerHTML = `<p>本页面还在建设中，欢迎参与完善！</p>`;
-}
-*/
